b2841d0093cf47a9e642b29ee014f96d12708989
gloomyline <1211071880@qq.com>
2017-05-27 03:43:36 +0800

    complete the tab

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
+  <div class="app">
+    <v-header class="header"></v-header>
     <div class="tab">
       <div class="tab-item">
-        <a v-link="{path: '/goods'}">商品</a>
+        <a v-link="{path: '/goods'}" class="tab-button">商品</a>
       </div>
       <div class="tab-item">
-        <a v-link="{path: '/ratings'}">评论</a>
+        <a v-link="{path: '/ratings'}" class="tab-button">评论</a>
       </div>
       <div class="tab-item">
-        <a v-link="{path: '/seller'}">商家</a>
+        <a v-link="{path: '/seller'}" class="tab-button">商家</a>
       </div>
     </div>
     <router-view></router-view>
@@ -16,9 +17,30 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default{}
+  import header from 'components/header'
+
+  export default{
+    components: {
+      'v-header': header
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin"
 
+  .app
+    .tab
+      display flex
+      height 40px
+      border-1px(rgba(7, 17, 27, .1))
+      .tab-item
+        flex 1
+        line-height 40px
+        text-align center
+        color rgb(77, 85, 93)
+        & > .tab-button
+          display block
+          &.active
+            color rgb(240, 20, 20)
 </style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>这里是头部</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
